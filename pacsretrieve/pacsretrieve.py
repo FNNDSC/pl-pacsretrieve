43b514704db655a0450941686f17821048479510
Rudolph Pienaar <rudolph.pienaar@gmail.com>
2017-10-24 09:04:21 -0400

    WIP shifting to new design

--- pacsretrieve/pacsretrieve.py
+++ pacsretrieve/pacsretrieve.py
@@ -1,5 +1,5 @@
 #                                                            _
-# Pacs query app
+# Pacs retrieve app
 #
 # (c) 2016 Fetal-Neonatal Neuroimaging & Developmental Science Center
 #                   Boston Children's Hospital
@@ -8,39 +8,497 @@
 #                        dev@babyMRI.org
 #
 
+str_version = "1.0.0"
 
+str_name = """
+    NAME
+
+        pacsretrieve.py
+"""
+str_synposis = """
+
+    SYNOPSIS
+
+        pacsretrieve.py --pfdcm <PACserviceIP:port>             \\
+                        [--version]                             \\
+                        [--msg <jsonMsgString>]                 \\
+                        [--priorHitsTable <hitsTable>]          \\
+                        [--indexList <commaseparatedlist>]      \\
+                        <inputdir>
+                        <outputdir>
+"""
+str_description = """
+
+    DESCRIPTION
+
+    'pacsretrieve.py' is a "DataService" (DS) ChrisApp plugin that is used
+    to retrieve data from a PACS.
+
+    Importantly, this app does *not* actually talk to a PACS directly;
+    instead it interacts with an intermediary service, typically 'pfdcm'.
+    This intermediary service actually connects to a PACS and performs
+    retrieves. This app can poll the service for status of a retrieve.
+
+    Thus, it is important to understand that this app does not need 
+    specific details on the PACS IP, port, AETITLE, etc. All of this
+    information is managed by 'pfdcm'. This does mean of course, that
+    'pfdcm' needs to be intantiated correctly. Please see the 'pfdcm'
+    github wiki for specific instructions. 
+
+    Note though that it is possible to pass to this app a 'pfdcm' 
+    compliant message string using the [--msg <jsonMsgString>]. This
+    <jsonMsgString> can be used to set 'pfdcm' internal lookup and 
+    add new PACS entries. This <jsonMsgString> can also be used to 
+    perform a retrieve.
+
+    However, most often, the simplest mechanism of retrieve will be through
+    the --priorHitsTable and --indexList command line flags that rely
+    on the output of a previously run 'pacsretrieve.py' plugin.
+
+    The <input> positional argument is MANDATORY and defines
+    the output directory (or relative dir when called through the
+    CHRIS API) of a previously run 'pacsretrieve.py' plugin that contains
+    a priorHitsTable. 
+
+    The <outputdir> positional argument is MANDATORY and defines
+    the output directory (or relative dir when called through the
+    CHRIS API) for the resultant DICOM files.
+    
+"""
+str_results = """
+
+    RESULTS
+
+    Results from this app are DICOM files in the <outputdir>
+
+"""
+str_args = """
+
+    ARGS
+
+    --pfdcm <PACserviceIP:port> 
+
+        The IP and port specifier of the 'pfdcm' service. 
+
+    --msg <jsonMsgString>    
+
+        A 'pfdcm' conforming message string. If sent to this app,
+        the message string is passed through unaltered to 'pfdcm'.
+        This allows for setting up internals of 'pfdcm' and/or
+        doing queries and interactions directly. 
+
+        USE WITH CARE.
+
+    --PACSservice <PACSservice> 
+
+        The "name" of the PACS to retrieve within 'pfdcm'. This is 
+        used to look up the PACS IP, port, AETitle, etc.
+
+    --priorHitsTable <hitsTable>    
+
+        The JSON table of hits from a prior call to pacsretrieve.
+
+    --indexList <commaseparatedlist>      
+
+        A comma separated list of series in the <hitsTable> to actually
+        retrieve.
+
+    <inputdir>
+
+        The input directory of a previous 'pacsretrieve.py' run.
+
+    <outputdir>
+
+        The output directory.
+
+"""
 
 import os
+import sys
 import json
-import time
-import shutil
-import subprocess
+import pprint
 import pypx
+import pfurl
+import pfmisc
+import pudb
+import shutil
+
 
 # import the Chris app superclass
 from chrisapp.base import ChrisApp
 
-# dicom settings
-#
-DICOM = {}
-DICOM['server_ip'] = '173.48.120.248'
-DICOM['server_port'] = '4242'
-DICOM['called_aet'] = 'ORTHANC'
-DICOM['calling_aet'] = 'CHIPS'
-DICOM['dicom_data'] = '/incoming/data/'
-
-if 'DICOM_SERVER_IP' in os.environ:
-    DICOM['server_ip'] = os.environ['DICOM_SERVER_IP']
-if 'DICOM_SERVER_PORT' in os.environ:
-    DICOM['server_port'] = os.environ['DICOM_SERVER_PORT']
-if 'CALLING_AET' in os.environ:
-    DICOM['calling_aet'] = os.environ['DICOM_CALLING_AET']
-if 'CALLED_AET' in os.environ:
-    DICOM['called_aet'] = os.environ['CALLED_AET']
-if 'DICOM_DATA' in os.environ:
-    DICOM['dicom_data'] = os.environ['DICOM_DATA']
-
 class PacsRetrieveApp(ChrisApp):
+    AUTHORS = 'FNNDSC (dev@babyMRI.org)'
+    SELFPATH = os.path.dirname(__file__)
+    SELFEXEC = os.path.basename(__file__)
+    EXECSHELL = 'python3'
+    TITLE = 'PACS Retrieve'
+    CATEGORY = ''
+    TYPE = 'ds'
+    DESCRIPTION = 'An app to initiate a retrieve on series of interest'
+    DOCUMENTATION = 'http://wiki'
+    LICENSE = 'Opensource (MIT)'
+    VERSION = '1.0.0'
+
+    # Fill out this with key-value output descriptive info (such as an output file path
+    # relative to the output dir) that you want to save to the output meta file when
+    # called with the --saveoutputmeta flag
+    OUTPUT_META_DICT = {}
+    
+    def __init__(self, *args, **kwargs):
+        ChrisApp.__init__(self, *args, **kwargs)
+
+        self.__name__           = 'PACSRetrieveApp'
+
+        # Debugging control
+        self.b_useDebug         = False
+        self.str_debugFile      = '/dev/null'
+        self.b_quiet            = True
+        self.dp                 = pfmisc.debug(    
+                                            verbosity   = 0,
+                                            level       = -1,
+                                            within      = self.__name__
+                                            )
+        self.pp                 = pprint.PrettyPrinter(indent=4)
+
+        # Input/Output dirs
+        self.str_inputDir       = ''
+        self.str_outputDir      = ''
+
+        # Service and payload vars
+        self.str_pfdcm          = ''
+        self.str_msg            = ''
+        # list of all retrieve commands
+        self.l_dmsg             = []
+        # a single retrieve command
+        self.d_msg              = {}
+
+        # Alternate, simplified CLI flags
+        self.str_priorHitsTable = ''
+        self.l_indexList        = []
+
+        # Control
+        self.b_canRun           = False
+
+        # Prior hits JSON dictionary
+        self.d_query            = {}
+
+        # Summary report
+        self.b_summaryReport    = False
+        self.str_summaryKeys    = ''
+        self.l_summaryKeys      = []
+        self.str_summaryFile    = ''
+
+        # Result report
+        self.str_resultFile     = ''
+       
+    def define_parameters(self):
+        """
+        Define the CLI arguments accepted by this plugin app.
+        """
+
+        # The space of input parameters is very straightforward
+        #   1. The IP:port of the pfdcm service
+        #   2. A 'msg' type string / dictionary to send to the service.
+
+        # PACS settings
+        self.add_argument(
+            '--pfdcm',
+            dest        = 'str_pfdcm',
+            type        = str,
+            default     = '',
+            optional    = True,
+            help        = 'The PACS Q/R intermediary service IP:port.')
+        self.add_argument(
+            '--msg',
+            dest        = 'str_msg',
+            type        = str,
+            default     = '',
+            optional    = True,
+            help        = 'The actual complete JSON message to send to the Q/R intermediary service.')
+        self.add_argument(
+            '--indexList',
+            dest        = 'str_indexList',
+            type        = str,
+            default     = '',
+            optional    = True,
+            help        = 'A comma separated list of indices into the priorHitsTable.')
+        self.add_argument(
+            '--priorHitsTable',
+            dest        = 'str_priorHitsTable',
+            type        = str,
+            default     = '',
+            optional    = True,
+            help        = 'A JSON formatted file returned by a prior call to pacsquery.')
+        self.add_argument(
+            '--PACSservice',
+            dest        = 'str_PACSservice',
+            type        = str,
+            default     = 'orthanc',
+            optional    = True,
+            help        = 'The PACS service to use. Note this a key to a lookup in "pfdcm".')
+        self.add_argument(
+            '--summaryKeys',
+            dest        = 'str_summaryKeys',
+            type        = str,
+            default     = '',
+            optional    = True,
+            help        = 'If specified, generate a summary report based on a comma separated key list.')
+        self.add_argument(
+            '--summaryFile',
+            dest        = 'str_summaryFile',
+            type        = str,
+            default     = '',
+            optional    = True,
+            help        = 'If specified, save (overwrite) a summary report to passed file (in outputdir).')
+        self.add_argument(
+            '--numberOfHitsFile',
+            dest        = 'str_numberOfHitsFile',
+            type        = str,
+            default     = '',
+            optional    = True,
+            help        = 'If specified, save (overwrite) the number of hits (in outputdir).')
+        self.add_argument(
+            '--resultFile',
+            dest        = 'str_resultFile',
+            type        = str,
+            default     = '',
+            optional    = True,
+            help        = 'If specified, save (overwrite) all the hits to the passed file (in outputdir).')
+        self.add_argument(
+            '--man',
+            dest        = 'str_man',
+            type        = str,
+            default     = '',
+            optional    = True,
+            help        = 'If specified, print help on the passed key entry. Use "entries" for all key list')
+        self.add_argument(
+            '--pfurlQuiet',
+            dest        = 'b_pfurlQuiet',
+            type        = bool,
+            default     = False,
+            action      = 'store_true',
+            optional    = True,
+            help        = 'Silence pfurl noise.'),
+        self.add_argument(
+            '--version',
+            dest        = 'b_version',
+            type        = bool,
+            default     = False,
+            action      = 'store_true',
+            optional    = True,
+            help        = 'Show .')
+
+    def df_print(self, adict):
+        """
+        Return a nicely formatted string representation of a dictionary
+        """
+        return self.pp.pformat(adict).strip()
+
+    def service_call(self, *args, **kwargs):
+
+        d_msg   = {}
+        for k, v in kwargs.items():
+            if k == 'msg':  d_msg   = v
+
+        serviceCall = pfurl.Pfurl(
+            msg                     = json.dumps(d_msg),
+            http                    = self.str_pfdcm,
+            verb                    = 'POST',
+            b_raw                   = True,
+            b_quiet                 = self.b_pfurlQuiet,
+            b_httpResponseBodyParse = True,
+            jsonwrapper             = 'payload',
+            debugFile               = self.str_debugFile,
+            useDebug                = self.b_useDebug
+        )
+        
+        self.dp.qprint('Sending d_msg =\n %s' % self.df_print(d_msg))
+        d_response      = json.loads(serviceCall())
+        return d_response
+
+    def man_get(self):
+        """
+        return a simple man/usage paragraph.
+        """
+
+        d_ret = {
+            "man":  str_name + str_synposis + str_description + str_results + str_args,
+            "synopsis":     str_synposis,
+            "description":  str_description,
+            "results":      str_results,
+            "args":         str_args,
+            "overview": """
+            """,
+            "callingSyntax1": """
+                python3 pacsretrieve.py --pfdcm ${HOST_IP}:5015 --msg \
+                '{  
+                    "action": "PACSinteract",
+                    "meta": 
+                        {
+                            "do":  "retrieve",
+                            "on" : 
+                            {
+                                "series_uid": "<someSeriesUID>"
+                            },
+                            "PACS" : "orthanc"
+                        }
+                }' /tmp
+            """,
+            "callingSyntax2": """
+                python3 pacsretrieve.py --pfdcm ${HOST_IP}:5015         \\
+                                        --PatientID LILLA-9731          \\
+                                        --PACSservice orthanc
+            """,
+            "callingSyntax3": """
+                python3 pacsretrieve.py --pfdcm ${HOST_IP}:5015         \\
+                                        --PACSservice orthanc           \\
+                                        --pfurlQuiet                    \\
+                                        --priorHitsTable results.json   \\
+                                        --indexList 1,2,3               \\
+                                        /tmp/query                      \\
+                                        /tmp/data
+            """
+        }
+
+        return d_ret
+
+    def queryTable_read(self, *args, **kwargs):
+        """
+        Read a JSON formatted query table generated by 'pacsquery'.
+        """
+
+        d_results       = {}
+        options         = None
+        for k,v in kwargs.items():
+            if k == 'priorHitsTable':   self.str_priorHitsTable = v
+
+        if len(self.str_priorHitsTable):
+            str_FQresultFile    = os.path.join(self.str_inputDir, self.str_priorHitsTable)
+            self.dp.qprint('Reading prior data results from %s' % str_FQresultFile )
+            f = open(str_FQresultFile, 'r')
+            self.d_query = json.load(f)
+            f.close()
+
+    def manPage_checkAndShow(self, options):
+        """
+        Check if the user wants inline help. If so, present requested help
+
+        Return a bool based on check.
+        """
+
+        ret = False
+        if len(options.str_man):
+            ret = True
+            d_man = self.man_get()
+            if options.str_man in d_man:
+                str_help    = d_man[options.str_man]
+                print(str_help)
+            if options.str_man == 'entries':
+                print(d_man.keys())
+
+        return ret
+
+    def directMessage_checkAndConstruct(self, options):
+        """
+        Checks if user specified a direct message to the 'pfdcm' service, 
+        and if so, construct the message.
+
+        Return True/False accordingly
+        """
+
+        ret = False
+        if len(options.str_msg):
+            ret = True
+            self.str_msg        = options.str_msg
+            try:
+                self.d_msg      = json.loads(self.str_msg)
+                self.l_dmsg.append(self.d_msg)
+                self.b_canRun   = True
+            except:
+                self.b_canRun   = False
+        return ret
+
+    def retrieveMessage_checkAndConstruct(self, options):
+        """
+        Checks if user specified a retrieve from a pattern of command line flags,
+        and if so, construct the message.
+
+        Return True/False accordingly
+        """
+
+        if len(options.str_priorHitsTable) and len(options.str_indexList):
+            self.l_indexList = options.str_indexList.split(',')
+            self.str_PACSservice    = options.str_PACSservice
+            for series in self.l_indexList:
+                str_seriesUID       = self.d_query['query']['data'][int(series)]['SeriesInstanceUID']['value']
+                self.l_dmsg.append({
+                    'action':   'PACSinteract',
+                    'meta': {
+                        'do':   'retrieve',
+                        'on': {
+                            'series_uid': str_seriesUID
+                        },
+                        "PACS": self.str_PACSservice
+                    }
+                })
+            self.b_canRun   = True
+
+    def outputFiles_generate(self, options, hits, d_ret, l_data):
+        """
+        Check and generate output files.
+        """
+        if len(options.str_numberOfHitsFile):
+            self.numberOfHitsReport_process(
+                                        hits        = hits,
+                                        hitsFile    = options.str_numberOfHitsFile
+                                        )
+
+        if len(options.str_resultFile):
+            self.dataReport_process     (    
+                                        results     = d_ret,
+                                        resultFile  = options.str_resultFile
+                                        )
+
+        if len(options.str_summaryKeys):
+            self.summaryReport_process  ( 
+                                        data        = l_data,
+                                        summaryKeys = options.str_summaryKeys,
+                                        summaryFile = options.str_summaryFile
+                                        )
+       
+
+    def run(self, options):
+        """
+        Define the code to be run by this plugin app.
+        """
+
+        d_ret                   = {
+            'status': False
+        }
+        self.b_pfurlQuiet       = options.b_pfurlQuiet
+        self.str_outputDir      = options.outputdir
+        self.str_inputDir       = options.inputdir
+
+        if options.b_version:
+            print(str_version)
+
+        pudb.set_trace()
+
+        if not self.manPage_checkAndShow(options) and not options.b_version:
+            if len(options.str_pfdcm):
+                self.str_pfdcm      = options.str_pfdcm
+                if not self.directMessage_checkAndConstruct(options):
+                    self.queryTable_read(priorHitsTable = options.str_priorHitsTable)
+                    self.retrieveMessage_checkAndConstruct(options)
+
+                if self.b_canRun:
+                    for self.d_msg in self.l_dmsg:
+                        d_ret   = self.service_call(msg = self.d_msg)
+
+        return d_ret
+
+class PacsRetrieveAppOld(ChrisApp):
     '''
     Create file out.txt witht the directory listing of the directory
     given by the --dir argument.
