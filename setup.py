69b0a16f46ac25f90b9a14c570385593cbe64869
Rudolph Pienaar <rudolph.pienaar@gmail.com>
2017-11-08 15:12:15 -0500

    Update to latest requirements and 1.1.1

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def readme():
 
 setup(
       name             =   'pacsretrieve',
-      version          =   '1.1.0',
+      version          =   '1.1.1',
       description      =   'Interact with a PACS through an intermediary service, "pfdcm" (not included).',
       long_description =   readme(),
       author           =   'Rudolph Pienaar',
